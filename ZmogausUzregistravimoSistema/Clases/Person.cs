d7420e31897b6bb76e69ad49e471afc81d2feaf1
DariusB79 <d.balsevicius@gmail.com>
2023-01-12 18:38:26 +0200

    Add project files.

---
+++ ZmogausUzregistravimoSistema/Clases/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace ZmogausUzregistravimoSistema.Clases
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public int PersonIdCode { get;}
+
+        public int PhoneNr { get; set; }
+
+        public string Email { get; set; }
+
+        public string Picture { get; set; }
+
+        public List<Place> Places = new List<Place>();  
+    }
+}
---
+++ ZmogausUzregistravimoSistema/Clases/Place.cs
@@ -0,0 +1,18 @@
+﻿namespace ZmogausUzregistravimoSistema.Clases
+{
+    public class Place
+    {
+        public int Id { get; set; } 
+
+        public int PersonId { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public int HomeNr { get; set; }
+
+        public int FlatNr { get; set; }
+
+    }
+}
